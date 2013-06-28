8bbaac48d34252fbd207a3efd014858825895b9e
datatab <deanchugall@gmail.com>
2013-06-28 02:26:11 +0200

    Implementacija Modula

---
+++ src/org/lugons/libre/lucene/rawdokumenta/RawDokumenta.java
@@ -0,0 +1,52 @@
+package org.lugons.libre.lucene.rawdokumenta;
+ 
+import java.io.*;
+import java.util.ArrayList;
+ 
+public class RawDokumenta {
+ 
+	private ArrayList<File> listaFajlova = new ArrayList<File>();
+ 
+	public RawDokumenta(String putanja) {
+		pronadjiFajlove(new File(putanja));
+	}
+ 
+	public ArrayList<File> getListaFajlova() {
+		return listaFajlova;
+	}
+ 
+	private void pronadjiFajlove(File file) {
+		// ako fascikla ili datoteka ne postoje.
+		if (!file.exists()) {
+			System.out.println(file + " ne postoji.");
+		}
+		// ako je fascikla - Recursion 
+		if (file.isDirectory()) {
+			for (File f : file.listFiles()) {
+				pronadjiFajlove(f);
+			}
+		} else {
+			String imeFajla = file.getName().toLowerCase();
+			// ===================================================
+			// Samo pronađi PDF datoteke
+			// ===================================================
+			if (imeFajla.endsWith(".pdf")) {
+				// System.out.println("Nađen fajl: " + file.getName());
+				getListaFajlova().add(file);
+			} else {
+				// System.out.println("Presko�?eno " + filename);
+			}
+		}
+ 
+	}
+ 
+	public static void main(String[] args) throws IOException {
+ 
+		System.out.println("Unesite putanju do direktorijuma ili fajla: (npr. /tmp/Biblioteka ili c:\\temp\\Biblioteka)");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String unosKorisnika = br.readLine();
+		RawDokumenta raw = new RawDokumenta(unosKorisnika);
+ 
+		System.out.println("Pronađeni fajlovi:  " + raw.getListaFajlova());
+	}
+}
---
+++ src/org/lugons/libre/lucene/tika/TikaEkstrakcija.java
@@ -0,0 +1,66 @@
+package org.lugons.libre.lucene.tika;
+
+import org.lugons.libre.lucene.rawdokumenta.RawDokumenta;
+import java.io.*;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.pdf.PDFParser;
+import org.apache.tika.sax.BodyContentHandler;
+
+import org.xml.sax.ContentHandler;
+
+public class TikaEkstrakcija {
+
+	public void tikaEkstrakt(String datoteka) throws IOException {
+
+		ContentHandler nosacSadrzaja;
+		InputStream is = null;
+
+		try {
+
+			is = new FileInputStream(datoteka);
+			nosacSadrzaja = new BodyContentHandler();
+			Metadata metadata = new Metadata();
+			PDFParser pdfParser = new PDFParser();
+			pdfParser.parse(is, nosacSadrzaja, metadata, new ParseContext());
+
+			System.out.println(nosacSadrzaja.toString());
+			sviMetapodaci(metadata);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (is != null)
+				is.close();
+		}
+
+	}
+
+	public void sviMetapodaci(Metadata metadata) {
+		for (int i = 0; i < metadata.names().length; i++) {
+			String name = metadata.names()[i];
+			System.out.println(name + " : " + metadata.get(name));
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("Unesite putanju do direktorijuma ili fajla:" +
+				" (npr. /tmp/Biblioteka ili c:\\temp\\Biblioteka)");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String unosKorisnika = br.readLine();
+		RawDokumenta raw = new RawDokumenta(unosKorisnika);
+
+		System.out.println("Pronađeni fajlovi:  " + raw.getListaFajlova());
+		System.out.println("============================================");
+		System.out.println("||           TIKA EKSTRAKCIJA             ||");
+		System.out.println("============================================");
+		System.out.println("");
+		TikaEkstrakcija tikaExt = new TikaEkstrakcija();
+		for (File file : raw.getListaFajlova()) {
+			System.out.println("Nađen fajl: " + file.getName());
+			tikaExt.tikaEkstrakt(file.getAbsolutePath());
+		}
+
+	}
+
+}
