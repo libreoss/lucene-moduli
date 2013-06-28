a84005a29aa78a4bd416159535ebb0ba0178c4d6
DeanChugall <deanchugall@gmail.com>
2013-06-28 21:01:18 +0200

    Tika sredjivanje

--- src/org/lugons/libre/lucene/tika/TikaEkstrakcija.java
+++ src/org/lugons/libre/lucene/tika/TikaEkstrakcija.java
@@ -13,15 +13,17 @@ public class TikaEkstrakcija {
 
 	public void tikaEkstrakt(String datoteka) throws IOException {
 
-		ContentHandler nosacSadrzaja;
 		InputStream is = null;
+		ContentHandler nosacSadrzaja;
+		Metadata metadata;
+		PDFParser pdfParser;
 
 		try {
 
 			is = new FileInputStream(datoteka);
 			nosacSadrzaja = new BodyContentHandler();
-			Metadata metadata = new Metadata();
-			PDFParser pdfParser = new PDFParser();
+			metadata = new Metadata();
+			pdfParser = new PDFParser();
 			pdfParser.parse(is, nosacSadrzaja, metadata, new ParseContext());
 
 			System.out.println(nosacSadrzaja.toString());
@@ -33,7 +35,6 @@ public class TikaEkstrakcija {
 			if (is != null)
 				is.close();
 		}
-
 	}
 
 	public void sviMetapodaci(Metadata metadata) {
@@ -44,20 +45,24 @@ public class TikaEkstrakcija {
 	}
 
 	public static void main(String[] args) throws IOException {
-		System.out.println("Unesite putanju do direktorijuma ili fajla:" +
-				" (npr. /tmp/Biblioteka ili c:\\temp\\Biblioteka)");
+		System.out.println("Unesite putanju do direktorijuma ili fajla:"
+				+ " (npr. /tmp/Biblioteka ili c:\\temp\\Biblioteka)");
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String unosKorisnika = br.readLine();
 		RawDokumenta raw = new RawDokumenta(unosKorisnika);
+		TikaEkstrakcija tikaExt = new TikaEkstrakcija();
 
 		System.out.println("Pronađeni fajlovi:  " + raw.getListaFajlova());
 		System.out.println("============================================");
 		System.out.println("||           TIKA EKSTRAKCIJA             ||");
 		System.out.println("============================================");
 		System.out.println("");
-		TikaEkstrakcija tikaExt = new TikaEkstrakcija();
+		System.out.println("============================================");
+		System.out.println("||               METAPODACI               ||");
+		System.out.println("============================================");
+
 		for (File file : raw.getListaFajlova()) {
-			System.out.println("Nađen fajl/ovi: " + file.getName());
+			System.out.println("Nađeni fajl-ovi: " + file.getName());
 			tikaExt.tikaEkstrakt(file.getAbsolutePath());
 		}
 
